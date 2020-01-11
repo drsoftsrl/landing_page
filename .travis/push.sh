729baa2e5ae50d2cd0fa933fd6c4114a85d5f2f7
Travis CI <travis@travis-ci.org>
2020-01-11 22:34:13 +0200

    travis build attempt debug

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "git push"
-cd ../
+ls -la
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
-git stash
 git fetch
+git stash
 git checkout ${TRAVIS_BRANCH}
 git stash pop
 git push https://${GH_TOKEN}@github.com/drsoftsrl/landing_page.git
