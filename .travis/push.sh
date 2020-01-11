92e17059ff9d372a73a659dff0dfd1b8cdbabfd9
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2020-01-11 10:54:07 +0200

    travis build attempt

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,6 +5,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
 git stash
+git fetch
 git checkout ${TRAVIS_BRANCH}
 git stash pop
 git push https://${GH_TOKEN}@github.com/drsoftsrl/landing_page.git
