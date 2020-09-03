4e22bff373ad3dbcc2eac5ec2d45539ef67e16a1
Simply Digital <68376859+DigitalSimply@users.noreply.github.com>
2020-09-03 10:37:02 -0700

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -4,27 +4,27 @@
  This is the link to your domain...it redirects the user to your myaccount page.
  It is used on the site on buttons like: Buy now, Order now, Members, Buy HTTP proxies, etc.
 */
-export const MEMBERS_URL = "https://members.url.goes.here";
+export const MEMBERS_URL = "https://myprofile.proxyservers.co";
 
 /*
  Your site name - this appears in the footer
 */
-export const SITE_NAME = "Site name goes here";
+export const SITE_NAME = "Proxy Servers Company";
 
 /*
  The name of you site that appears in Terms of service section - it can be you domain, your company name, etc.
 */
-export const TOS_SITE_NAME = 'whatever-you-need';
+export const TOS_SITE_NAME = 'Proxy Servers Company';
 
 /*
  The date when the terms of service where last updated.
 */
-export const TOS_LAST_UPDATE = 'Apr-27-2018';
+export const TOS_LAST_UPDATE = '09/1/2020';
 
 /*
  This is the link of your chat widget - it can be found on your tawk to account
 */
-export const TAWK_TO_WIDGET_URL = 'https://embed.tawk.to/5b61937de21878736ba285c5/default';
+export const TAWK_TO_WIDGET_URL = 'https://embed.tawk.to/5f4f6469f0e7167d000cc5b6/default';
 
 /*
  This is the link to your backend services. It is used to get the data for prices and datacenters. Normally, you shouldn't touch this one
@@ -34,7 +34,7 @@ export const BACKEND_URL = 'https://backend.changemyip.com';
 /*
  Meta tags for SEO - description and keywords for this site
 */
-export const SEO_DESCRIPTION = 'Fully anonymous private proxies (shared or dedicated)';
+export const SEO_DESCRIPTION = 'Dedicated and Shared Private Proxy and SOCKS Servers';
 // The keywords need to be separated by comma
-export const SEO_KEYWORDS = 'proxy, socks, shared proxies, dedicated proxies, residential proxies, socksv5';
+export const SEO_KEYWORDS = 'proxy, socks, shared proxies, dedicated proxies, residential proxies, socksv5, socks5, socks server, proxy server, proxies, proxyservers';
 
